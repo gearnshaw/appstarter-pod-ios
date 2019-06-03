e9b9549743133ef496c34d56dd22eb4204b31920
Gabrielle Earnshaw <g_earnshaw@hotmail.com>
2019-06-03 19:30:23 +0100

    Added the logging protocol

---
+++ appstarter-pod-ios/Classes/logging/Logging.swift
@@ -0,0 +1,16 @@
+//
+//  Logging.swift
+//  appstarter-pod-ios
+//
+//  Created by Gabrielle Earnshaw on 03/06/2019.
+//
+
+import Foundation
+
+/// A protocol to allow logging in the app, whilst hiding the actual logging implementation
+protocol Logging {
+    func debug(_ closure: @autoclosure () -> Any?)
+    func info(_ closure: @autoclosure () -> Any?)
+    func warn(_ closure: @autoclosure () -> Any?)
+    func error(_ closure: @autoclosure () -> Any?)
+}
