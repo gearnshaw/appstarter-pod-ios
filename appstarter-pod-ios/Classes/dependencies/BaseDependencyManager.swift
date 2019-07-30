45ba94ea0e2bd8ef571b2e139a2a1a5a8586bd3e
Gabrielle Earnshaw <g_earnshaw@hotmail.com>
2019-07-30 15:38:46 +0100

    Removed some of the dependency management classes that don't make sense.

---
+++ appstarter-pod-ios/Classes/dependencies/BaseDependencyManager.swift
@@ -0,0 +1,26 @@
+//
+//  BaseDependencyManager.swift
+//  appstarter-pod-ios
+//
+//  Created by Gabrielle Earnshaw on 08/07/2019.
+//
+
+import Foundation
+
+/// The class that handles dependency management
+open class BaseDependencyManager {
+
+    // Public initialiser
+    public init(){}
+    
+    // MARK:- Singleton variables
+//    public lazy var myString: String = {
+//        return ""
+//    }()
+
+    // MARK:- Factoried variables
+    public var sizeProvider: SizeProvider {
+        return SizeProvider()
+    }
+
+}
